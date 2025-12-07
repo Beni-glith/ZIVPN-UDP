e7da002288c04ba2326e0c7fc8ab5936b035feb2
Beni-glith <gedangsriping48@gmail.com>
2025-12-07 11:10:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -730,7 +730,7 @@ function show_expired_message_and_exit() {
     echo -e "${BOLD_WHITE}Akses ke layanan ZIVPN di server anda telah dihentikan."
     echo -e "Segala aktivitas VPN tidak akan berfungsi lagi.\n"
     echo -e "Untuk memperpanjang lisensi dan mengaktifkan kembali layanan,"
-    echo -e "silakan hubungi admin https://wa.me/6287777694482 \n"
+    echo -e "silakan hubungi admin https://wa.me/6281311967169 \n"
     echo -e "${LIGHT_GREEN}Setelah diperpanjang, layanan akan aktif kembali secara otomatis.${NC}\n"
     exit 0
 }
